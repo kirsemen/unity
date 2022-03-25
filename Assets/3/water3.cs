57616dd511bb96e4497051e509743d2c4dd799a3
semeon <79764144+kirsemen@users.noreply.github.com>
2022-03-25 10:58:21 +0300

    1

--- Assets/3/water3.cs
+++ Assets/3/water3.cs
@@ -1,48 +1,113 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
-public class water3 : MonoBehaviour
+
+public class Water3 : MonoBehaviour
 {
-    public CustomRenderTexture tex;
-    public CustomRenderTexture tex1;
+    public struct Pixel
+    {
+        public UnityEngine.Vector2 pos;
+        public UnityEngine.Vector2 vec;
+        public int inside;
+        public Pixel(float x, float y, float vecX, float vecY)
+        {
+            this.pos = new UnityEngine.Vector2(x, y);
+            this.vec = new UnityEngine.Vector2(vecX, vecY);
+            this.inside = 1;
+        }
+        public Pixel(UnityEngine.Vector2 pos, UnityEngine.Vector2 vec)
+        {
+            this.pos = pos;
+            this.vec = vec;
+            this.inside = 1;
+        }
+    }
+
     public ComputeShader computeShader;
-    public CustomRenderTexture t_tex;
-    public CustomRenderTexture t_tex1;
+    public ComputeShader computeShader1;
+    public CustomRenderTexture tex;
+    public CustomRenderTexture texture;
+    private Pixel[] date = new Pixel[131072];
+    private int arrSize = 0;
+    private int overflow = 0;
+    private ComputeBuffer computeBuffer;
 
     private void Start()
     {
-        t_tex = new CustomRenderTexture(tex.width, tex.height);
-        t_tex.enableRandomWrite = true;
-        t_tex1 = new CustomRenderTexture(tex.width, tex.height);
-        t_tex1.enableRandomWrite = true;
-        Color color = new Color(0, 0, 0, 0);
-        t_tex.initializationColor = color;
-        t_tex1.initializationColor = color;
+        tex = new CustomRenderTexture(texture.width, texture.height);
+        tex.enableRandomWrite = true;
+        tex.initializationColor = new Color(0, 0, 0, 0);
 
-        tex.Initialize();
-        tex1.Initialize();
+
+        computeBuffer = new ComputeBuffer(date.Length, sizeof(float) * 4 + sizeof(int));
+        computeBuffer.SetData(date);
+
     }
+
     private void Update()
     {
-        Graphics.CopyTexture(tex, t_tex);
-        Graphics.CopyTexture(tex1, t_tex1);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, -transform.up, out hit))
+        {
+            for (int x = -10; x < 10; x++)
+                for (int y = -10; y < 10; y++)
+                    if (arrSize < date.Length)
+                    {
+                        date[arrSize] = new Pixel(hit.textureCoord * new UnityEngine.Vector2(texture.width, texture.height) + new UnityEngine.Vector2(x, y), new UnityEngine.Vector2(1, 1));
+                        arrSize++;
+                    }
+                    else
+                    {
+                        date[overflow] = new Pixel(hit.textureCoord * new UnityEngine.Vector2(texture.width, texture.height) + new UnityEngine.Vector2(x, y), new UnityEngine.Vector2(1, 1));
+                        overflow++;
+                        overflow %= date.Length;
+                    }
+            computeBuffer.SetData(date);
+        }
+        if (arrSize != 0)
+        {
 
-        computeShader.SetTexture(0, "tex", t_tex);
-        computeShader.SetTexture(0, "tex1", t_tex1);
-        computeShader.Dispatch(0, tex.width, tex.height, 1);
 
-        Graphics.CopyTexture(t_tex, tex);
-        Graphics.CopyTexture(t_tex1, tex1);
-        t_tex.Initialize();
-        t_tex1.Initialize();
+            computeShader.SetBuffer(0, "date", computeBuffer);
+            computeShader.SetInt("size", arrSize);
+            computeShader.SetVector("SizeMap", new UnityEngine.Vector2(texture.width, texture.height));
+            computeShader.Dispatch(0, date.Length/1024, 1, 1);
 
+
+            computeShader1.SetBuffer(0, "date", computeBuffer);
+            computeShader1.SetInt("size", arrSize);
+            computeShader1.SetTexture(0, "tex", tex);
+            computeShader1.Dispatch(0, date.Length/1024, 1, 1);
+
+            Graphics.CopyTexture(tex, texture);
+
+
+            Pixel[] newDate = new Pixel[date.Length];
+
+            newDate.CopyTo(date, 0);
+            int oldArrSize = arrSize;
+            arrSize = 0;
+
+            computeBuffer.GetData(newDate);
+
+            for (int i = 0; i < oldArrSize; i++)
+            {
+                if (newDate[i].inside == 1)
+                {
+                    date[arrSize] = newDate[i];
+                    arrSize++;
+                }
+            }
+            computeBuffer.SetData(date);
+
+        }
+        tex.Initialize();
     }
-
-
     private void OnDestroy()
     {
-        tex.Initialize();
-        tex1.Initialize();
+        computeBuffer.Dispose();
+        texture.Initialize();
     }
 }
